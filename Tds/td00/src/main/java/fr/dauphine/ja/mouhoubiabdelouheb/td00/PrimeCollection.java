14d0833f0459be8776318991fe3d5fde270b795a
mouhab19 <mouhab19@b028-21.crio.dauphine.fr>
2019-10-07 13:50:08 +0200

    Mise a jour de l'arborescence des tds et finalisation du td00.

---
+++ Tds/td00/src/main/java/fr/dauphine/ja/mouhoubiabdelouheb/td00/PrimeCollection.java
@@ -0,0 +1,59 @@
+
+
+package fr.dauphine.ja.mouhoubiabdelouheb.td00;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+/**
+ * Hello world!
+ *
+ */
+public class PrimeCollection 
+{
+	private ArrayList<Integer> numbers;
+	private Random random=new Random();
+	
+	public PrimeCollection() {
+		numbers=new ArrayList<Integer>();
+	}
+	
+	public void initRandom(int n,int m)
+	{
+		for (int i = 0; i < n; i++) {
+			int v=random.nextInt(m);
+			numbers.add(v);
+		}
+	}
+	
+	public boolean isPrime(int p)
+	{
+		if(p<1)
+		return false;
+		
+		for (int i = 2; i < Math.sqrt(p); i++) {
+			if(p%i == 0)
+				return false;
+			
+		}
+		
+		return true;
+	}
+	
+	public void printPrimes()
+	{
+		for (Integer e : this.numbers) {
+			if(isPrime(e))
+				System.out.println(e);
+		}
+	}
+	
+	
+    public static void main( String[] args )
+    {
+        PrimeCollection p=new PrimeCollection();
+        p.initRandom(100, 2000);
+        p.printPrimes();
+        
+    }
+}
---
+++ Tds/td00/src/test/java/fr/dauphine/ja/mouhoubiabdelouheb/td00/PrimeCollectionTest.java
@@ -0,0 +1,60 @@
+package fr.dauphine.ja.mouhoubiabdelouheb.td00;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class PrimeCollectionTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public PrimeCollectionTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( PrimeCollectionTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testPrimeCollection()
+    {
+        assertTrue( true );
+    }
+    
+    public void test0IsPrime()
+    {
+        PrimeCollection p=new PrimeCollection();
+    	
+        assertFalse( p.isPrime(0) );
+    }
+    
+    public void testTwoIsPrime()
+    {
+    	PrimeCollection p=new PrimeCollection();
+    	
+        assertTrue( p.isPrime(2) );
+    }
+    
+    public void test9IsNotPrime()
+    {
+    	PrimeCollection p=new PrimeCollection();
+    	
+        assertTrue(p.isPrime(9));
+    }
+    
+}
